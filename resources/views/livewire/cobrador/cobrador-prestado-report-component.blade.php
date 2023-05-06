00ba6337e7a25a9b2b97299bf27dc1e861f9563b
rodrigo55 villaortiz <villaortiz110@gmail.com>
2023-05-06 09:17:11 -0400

    cambios

--- resources/views/livewire/cobrador/cobrador-prestado-report-component.blade.php
+++ resources/views/livewire/cobrador/cobrador-prestado-report-component.blade.php
@@ -46,13 +46,25 @@
         </div>
     </div>
     @push('modals')
-        <div class="modal fade" id="modalVerPrestamoSemana" tabindex="-1" aria-labelledby="modalVerPrestamoSemanaLabel"
+        <div class="modal fade" id="modalVerPrestamoSemanaMany" tabindex="-1" aria-labelledby="modalVerPrestamoSemanaLabel"
             style="display: none;" aria-hidden="true" wire:ignore.self>
             <div class="modal-dialog modal-md modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-body text-center">
                         <h3 class="text-color-theme mb-2 size-14">Prestamos de la semana</h3>
-                        <ul class="list-group list-group-flush bg-none lista-prestamo">
+                        <ul class="list-group list-group-flush bg-none lista-prestamo-many">
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="modalVerPrestamoSemanaSingle" tabindex="-1" aria-labelledby="modalVerPrestamoSemanaLabel"
+            style="display: none;" aria-hidden="true" wire:ignore.self>
+            <div class="modal-dialog modal-md modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body text-center">
+                        <h3 class="text-color-theme mb-2 size-14">Prestamos de la semana</h3>
+                        <ul class="list-group list-group-flush bg-none lista-prestamo-single">
                         </ul>
                     </div>
                 </div>
@@ -62,74 +74,73 @@
 
 
     @push('footer')
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-            <script>
-                function ucfirst(string) {
-                    return string.charAt(0).toUpperCase() + string.slice(1);
-                }
-                Livewire.on('mostrarSemanaMany', data => {
-                    $('#modalVerPrestamoSemana').modal('show');
-                    $('.lista-prestamo').empty();
-                    var diaAnterior = null;
-                    data.forEach(function(prestamo, index) {
-                        var fecha = prestamo.created_at;
-                        if (prestamo.nombreDia != diaAnterior) {
-                            $('.lista-prestamo').append(
-                                '<span class="size-15" style="margin-top: 3%; display: flex; align-items: flex-start;justify-content: flex-start;">' +
-                                prestamo.nombreDia + '</span>');
-                            diaAnterior = prestamo.nombreDia;
-                        }
-                        $('.lista-prestamo').append(
-                            '<li class="list-group-item p-1 m-1"><div class="row"><div class="col d-flex align-self-center ps-0"><div class="avatar avatar-40 rounded-10" style="margin-right:3%;"><img src="{{ asset('') }}' +
-                            prestamo.user.foto +
-                            '" alt=""></div><div class="col align-self-center ps-0"><p align="left" style="text-align: left !important;" class="text-color-theme mb-0 size-10">' +
-                            prestamo.user.name + ' ' + prestamo.user.apellido +
-                            '</p><p align="left" style="text-align: left !important;margin-bottom: 0px;margin-top: 3px;" class="text-muted size-12"> CI : ' +
-                            prestamo.user.ci +
-                            '</p>  <p align="left" style="text-align: left !important;" class="text-muted size-12"> Couta : ' +
-                            prestamo.cuota +
-                            ' Bs.</p>  </div><div class="col align-self-center text-end"><p class="mb-0 size-12">' +
-                            prestamo.monto_inicial + ' Bs.</p><p class="text-muted size-12">' + moment(fecha)
-                            .format('H:mm A') + '</p></div></div></div></li>');
-                    });
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+        <script>
+            function ucfirst(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
+            Livewire.on('mostrarSemanaMany', data => {
+                $('#modalVerPrestamoSemanaMany').modal('show');
+                $('.lista-prestamo-many').empty();
+                var diaAnterior = null;
+                data.forEach(function(prestamo, index) {
+                    var fecha = prestamo.created_at;
+                    if (prestamo.nombreDia != diaAnterior) {
+                        $('.lista-prestamo-many').append(
+                            '<span class="size-15" style="margin-top: 3%; display: flex; align-items: flex-start;justify-content: flex-start;">' +
+                            prestamo.nombreDia + '</span>');
+                        diaAnterior = prestamo.nombreDia;
+                    }
+                    $('.lista-prestamo-many').append(
+                        '<li class="list-group-item p-1 m-1"><div class="row"><div class="col d-flex align-self-center ps-0"><div class="avatar avatar-40 rounded-10" style="margin-right:3%;"><img src="{{ asset('') }}' +
+                        prestamo.user.foto +
+                        '" alt=""></div><div class="col align-self-center ps-0"><p align="left" style="text-align: left !important;" class="text-color-theme mb-0 size-10">' +
+                        prestamo.user.name + ' ' + prestamo.user.apellido +
+                        '</p><p align="left" style="text-align: left !important;margin-bottom: 0px;margin-top: 3px;" class="text-muted size-12"> CI : ' +
+                        prestamo.user.ci +
+                        '</p>  <p align="left" style="text-align: left !important;" class="text-muted size-12"> Couta : ' +
+                        prestamo.cuota +
+                        ' Bs.</p>  </div><div class="col align-self-center text-end"><p class="mb-0 size-12">' +
+                        prestamo.monto_inicial + ' Bs.</p><p class="text-muted size-12">' + moment(fecha)
+                        .format('H:mm A') + '</p></div></div></div></li>');
                 });
-            </script>
+            });
+        </script>
 
-            <script>
-                function ucfirst(string) {
-                    return string.charAt(0).toUpperCase() + string.slice(1);
-                }
-                Livewire.on('mostrarSemanaSingle', datas => {
-                    datas.sort(function(a, b) {
-                        return moment(b.created_at).unix() - moment(a.created_at).unix();
-                    });
-                    $('#modalVerPrestamoSemana').modal('show');
-                    $('.lista-prestamo').empty();
-                    var diaAnterior = null;
-                    datas.forEach(function(prestamo, index) {
-                        var fecha = prestamo.created_at;
-                        if (prestamo.nombreDia != diaAnterior) {
-                            $('.lista-prestamo').append(
-                                '<span class="size-15" style="margin-top: 3%; display: flex; align-items: flex-start;justify-content: flex-start;">' +
-                                prestamo.nombreDia + '</span>');
-                            diaAnterior = prestamo.nombreDia;
-                        }
-                        $('.lista-prestamo').append(
-                            '<li class="list-group-item p-1 m-1"><div class="row"><div class="col d-flex align-self-center ps-0"><div class="avatar avatar-40 rounded-10" style="margin-right:3%;"><img src="{{ asset('') }}' +
-                            prestamo.user.foto +
-                            '" alt=""></div><div class="col align-self-center ps-0"><p align="left" style="text-align: left !important;" class="text-color-theme mb-0 size-10">' +
-                            prestamo.user.name + ' ' + prestamo.user.apellido +
-                            '</p><p align="left" style="text-align: left !important;margin-bottom: 0px;margin-top: 3px;" class="text-muted size-12"> CI : ' +
-                            prestamo.user.ci +
-                            '</p>  <p align="left" style="text-align: left !important;" class="text-muted size-12"> Couta : ' +
-                            prestamo.cuota +
-                            ' Bs.</p>  </div><div class="col align-self-center text-end"><p class="mb-0 size-12">' +
-                            prestamo.monto_inicial + ' Bs.</p><p class="text-muted size-12">' + moment(fecha)
-                            .format('H:mm A') + '</p></div></div></div></li>');
-                    });
+        <script>
+            function ucfirst(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
+            Livewire.on('mostrarSemanaSingle', datas => {
+                datas.sort(function(a, b) {
+                    return moment(b.created_at).unix() - moment(a.created_at).unix();
                 });
-            </script>
-        
+                $('#modalVerPrestamoSemanaSingle').modal('show');
+                $('.lista-prestamo-single').empty();
+                var diaAnterior = null;
+                datas.forEach(function(prestamo, index) {
+                    var fecha = prestamo.created_at;
+                    if (prestamo.nombreDia != diaAnterior) {
+                        $('.lista-prestamo-single').append(
+                            '<span class="size-15" style="margin-top: 3%; display: flex; align-items: flex-start;justify-content: flex-start;">' +
+                            prestamo.nombreDia + '</span>');
+                        diaAnterior = prestamo.nombreDia;
+                    }
+                    $('.lista-prestamo-single').append(
+                        '<li class="list-group-item p-1 m-1"><div class="row"><div class="col d-flex align-self-center ps-0"><div class="avatar avatar-40 rounded-10" style="margin-right:3%;"><img src="{{ asset('') }}' +
+                        prestamo.user.foto +
+                        '" alt=""></div><div class="col align-self-center ps-0"><p align="left" style="text-align: left !important;" class="text-color-theme mb-0 size-10">' +
+                        prestamo.user.name + ' ' + prestamo.user.apellido +
+                        '</p><p align="left" style="text-align: left !important;margin-bottom: 0px;margin-top: 3px;" class="text-muted size-12"> CI : ' +
+                        prestamo.user.ci +
+                        '</p>  <p align="left" style="text-align: left !important;" class="text-muted size-12"> Couta : ' +
+                        prestamo.cuota +
+                        ' Bs.</p>  </div><div class="col align-self-center text-end"><p class="mb-0 size-12">' +
+                        prestamo.monto_inicial + ' Bs.</p><p class="text-muted size-12">' + moment(fecha)
+                        .format('H:mm A') + '</p></div></div></div></li>');
+                });
+            });
+        </script>
     @endpush
 
 
